ca8d3899c1fa7ffe3f364b2a2aced2139a436775
Keigo01 <keigosax@yahoo.co.jp>
2017-01-20 11:18:08 +0900

    実行可能jar

--- ZemiB/src/ZemiB.java
+++ ZemiB/src/ZemiB.java
@@ -1,9 +1,7 @@
 import org.eclipse.jdt.core.dom.CompilationUnit;
 
 public class ZemiB {
-
 	public static void main(final String[] args) {
-
 		final CompilationUnit unit = ZemiBASTVisitor.createAST(args[0]);
 		final ZemiBASTVisitor visitor = new ZemiBASTVisitor();
 		unit.accept(visitor);
